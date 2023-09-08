834a7f0ee809402eb3dc6524e7fef6f36c6bbad0
jupacear2003 <jpceballos35@misena.edu.co>
2023-09-07 23:04:47 -0500

    Api de insumos

--- resources/views/categorium/index.blade.php
+++ resources/views/categorium/index.blade.php
@@ -7,7 +7,7 @@ Categoria
 @section('content')
 <section class="section">
     <div class="section-header">
-        <h3 class="page__heading">Categoria</h3>
+        <h3 class="page__heading">Categorias</h3>
     </div>
     <div class="section-body">
         @if ($errors->any())
